d7e78ae80e704b7dba9c6665515b7b2c8b3fc9bb
Bruno Mesquita <brunocmesquita@gmail.com>
2021-08-28 18:50:53 -0300

    initial configuration. Added drawer and cleanup the project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, labore
+      praesentium ut dolorem earum distinctio! Doloremque obcaecati commodi a
+      similique placeat nesciunt aspernatur? Id in sunt suscipit, rem quas
+      sapiente.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, labore
+      praesentium ut dolorem earum distinctio! Doloremque obcaecati commodi a
+      similique placeat nesciunt aspernatur? Id in sunt suscipit, rem quas
+      sapiente.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, labore
+      praesentium ut dolorem earum distinctio! Doloremque obcaecati commodi a
+      similique placeat nesciunt aspernatur? Id in sunt suscipit, rem quas
+      sapiente.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, labore
+      praesentium ut dolorem earum distinctio! Doloremque obcaecati commodi a
+      similique placeat nesciunt aspernatur? Id in sunt suscipit, rem quas
+      sapiente.
+    </p>
   </div>
 </template>
