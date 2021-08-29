1c01cf0725e1a26f4338244079cd9a398c4b4c9d
Bruno Mesquita <brunocmesquita@gmail.com>
2021-08-29 11:09:07 -0300

    added child components

---
+++ src/components/Todo/FieldAddTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-text-field
+    v-model="newTaskTitle"
+    @click:append="addTask"
+    @keyup.enter="addTask"
+    class="pa-3"
+    outlined
+    hide-details
+    clearable
+    label="Add task"
+    append-icon="mdi-plus"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTaskTitle: '',
+    };
+  },
+  methods: {
+    addTask() {
+      this.$store.commit('addTask', this.newTaskTitle);
+      this.newTaskTitle = '';
+    },
+  },
+};
+</script>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,72 +1,18 @@
 <template>
   <div class="home">
-    <v-text-field
-      v-model="newTaskTitle"
-      @click:append="addTask"
-      @keyup.enter="addTask"
-      class="pa-3"
-      outlined
-      hide-details
-      clearable
-      label="Add task"
-      append-icon="mdi-plus"
-    ></v-text-field>
-    <v-list flat class="pt-0" v-if="$store.state.tasks.length">
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn @click.stop="$store.commit('deleteTask', task.id)" icon>
-                <v-icon color="primary">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
-    <div v-else class="no-tasks">
-      <v-icon size="100" color="primary">mdi-check</v-icon>
-      <div class="text-h5 primary--text">No tasks</div>
-    </div>
+    <field-add-task></field-add-task>
+    <list-tasks v-if="$store.state.tasks.length"></list-tasks>
+    <no-tasks v-else></no-tasks>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  data() {
-    return {
-      newTaskTitle: '',
-    };
-  },
-  methods: {
-    addTask() {
-      this.$store.commit('addTask', this.newTaskTitle);
-      this.newTaskTitle = '';
-    },
+  components: {
+    'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+    'list-tasks': require('@/components/Todo/ListTasks.vue').default,
+    'no-tasks': require('@/components/Todo/NoTasks.vue').default,
   },
 };
 </script>
-
-<style lang="sass">
-.no-tasks
-  position: absolute
-  left: 50%
-  top: 50%
-  transform: translate(-50%, -50%)
-  opacity: 0.5
-</style>
